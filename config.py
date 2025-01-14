c14bd966891b4482726aaa146b7ff9b97c32eb84
Keyur <89395991+keyursaxenacrossover@users.noreply.github.com>
2025-01-14 09:55:24 -0800

    using dynamic pathing

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
-docdir = r"C:\AGC\ag2_docs"
-chunkdir = r"C:\AGC\ag2_chunks"
-embeddir = r"C:\AGC\ag2_embeds"
-imagedir = r"C:\AGC\images"
-imagetodeletedir = r"C:\AGC\images_to_delete"
+import os
+
+# Get the directory where the current file (config.py) is located
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define paths relative to the base_dir
+docdir = os.path.join(base_dir, "ag2_docs")
+chunkdir = os.path.join(base_dir, "ag2_chunks")
+embeddir = os.path.join(base_dir, "ag2_embeds")
+imagedir = os.path.join(base_dir, "images")
+imagetodeletedir = os.path.join(base_dir, "images_to_delete")
